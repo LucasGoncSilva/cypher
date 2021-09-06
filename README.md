debdd20cd9feefa58e70f3eae2c18daa9e65dc8d
CapivaraMongolina <lucasgoncsilva04@gmail.com>
2021-09-06 16:06:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Descricao
+
+Baseado na "Cifra de Cesar" com algumas modificacoes pessoais, este programa com interface grafica possibilita a traducao ida e volta entre um texto/mensagem normal e cifrado.
+A matematica por tras da traducao eh simples o suficiente para que seja possivel traduzir mensagens sem a necessidade de um programa, utilizando apenas a propria mente. Com base nisso, o Cypher foi desenvolvido para facilitar e acelerar a transcricao de textos longos para troca de informacoes.
+
+
+# Utilizacao
+
+## Codificar e decodificar
+
+Para realizar ambas as operacoes (tanto para codificar quanto para decodificar) deve-se, apenas, inserir o texto no primeiro campo de entrada e clickar no botao que a "traducao" sera realizada automaticamente.
+
+
+# Cuidados e restricoes
+
+* O programa cifra apenas letras minusculas;
+* O programa nao cifra: letras maiusculas, numeros, simbolos e caracteres especiais (ainda eh possivel fazer uso dos elementos anteriormente citados apenas para embaralhar e esconder a mensagem original, contudo, tais elementos permanecerao intocados - em sua maioria);
+* Recomenda-se a escrita de nao mais que 60 caracteres para uma melhor visualizacao do que foi escrito (perfeitamente possivel a insercao de mais de 60 caracteres). 
---
+++ cypher.py
@@ -0,0 +1,76 @@
+from tkinter import *
+
+
+def cypher():
+    text = input_camp.get()
+    new_text = text.replace("a","A")
+    new_text = new_text.replace("c","a")
+    new_text = new_text.replace("d","D")
+    new_text = new_text.replace("e","E")
+    new_text = new_text.replace("f","F")
+    new_text = new_text.replace("g","G")
+    new_text = new_text.replace("h","H")
+    new_text = new_text.replace("i","I")
+    new_text = new_text.replace("j","J")
+    new_text = new_text.replace("k","K")
+    new_text = new_text.replace("l","L")
+    new_text = new_text.replace("m","M")
+    new_text = new_text.replace("n","N")
+    new_text = new_text.replace("p","n")
+    new_text = new_text.replace("q","m")
+    new_text = new_text.replace("r","l")
+    new_text = new_text.replace("s","k")
+    new_text = new_text.replace("t","j")
+    new_text = new_text.replace("u","i")
+    new_text = new_text.replace("v","h")
+    new_text = new_text.replace("w","g")
+    new_text = new_text.replace("x","f")
+    new_text = new_text.replace("y","e")
+    new_text = new_text.replace("z","d")
+    new_text = new_text.replace("A","c")
+    new_text = new_text.replace("D","z")
+    new_text = new_text.replace("E","y")
+    new_text = new_text.replace("F","x")
+    new_text = new_text.replace("G","w")
+    new_text = new_text.replace("H","v")
+    new_text = new_text.replace("I","u")
+    new_text = new_text.replace("J","t")
+    new_text = new_text.replace("K","s")
+    new_text = new_text.replace("L","r")
+    new_text = new_text.replace("M","q")
+    new_text = new_text.replace("N","p")
+
+    output_txt.set(f"{new_text}")
+
+
+win = Tk()
+win.title('Cypher')
+
+
+info = Label(win, text='INSIRA O TEXTO NA JANELA ABAIXO PARA (DES)CRIPTOGRAFAR')
+info.grid(column=0, row=0, padx=10, pady=5)
+
+
+rule1 = Label(win, text='*escreva sempre em minusculo')
+rule1.grid(column=0, row=2, padx=10, pady=2)
+
+
+rule2 = Label(win, text='*quantidade max indicada de carac. = 60')
+rule2.grid(column=0, row=4, padx=10, pady=2)
+
+
+input_camp = Entry(win, width=64)
+input_camp.grid(column=0, row=5, padx=10, pady=5)
+
+
+submit_button = Button(win, text='Converter texto', command=cypher)
+submit_button.grid(column=0, row=6, padx=10, pady=5)
+
+
+output_txt = StringVar()
+
+output_camp = Entry(win, textvariable=output_txt, state="readonly", width=64)
+output_camp.grid(column=0, row=8, padx=10, pady=5)
+
+
+win.mainloop()
